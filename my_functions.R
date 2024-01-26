14927b47fbc3dbb5bddff552582e907d85347773
odetaodeta <157810632+odetaodeta@users.noreply.github.com>
2024-01-26 15:01:40 +0100

    Add files via upload
    Added my functions

---
+++ my_functions.R
@@ -0,0 +1,31 @@
+#My Functions
+
+remind_me <- function() {
+  shopping_list <- c("Milk", "Bread", "Eggs", "Orange Juice")
+  return(shopping_list)
+}
+
+remind_me()
+
+
+
+cheat <- function(assignment_number) {
+  assignments <- list(
+    assignment3_1_7 = "data(cars) ggplot(cars, aes(x = speed, y = dist)) +
+  geom_point(pch = 16, col = 'black') + geom_smooth()",
+    assignment3_1_9 = "data(ToothGrowth) ggstatsplot::ggbetweenstats
+  (data= ToothGrowth, x= supp, y= len)",
+    assignment3_1_17 = "Ctrl + Shift + A"
+  )
+  assignment_name <- paste0("assignment", assignment_number)
+  if (assignment_name %in% names(assignments)){
+    return(assignments[[assignment_name]])
+  } else{
+    return("Assignment not found.")
+  }
+}
+
+assignment_code <- cheat("3_1_7")
+print(assignment_code)
+#"data(cars) ggplot(cars, aes(x = speed, y = dist)) +\n  
+#geom_point(pch = 16, col = 'black') + geom_smooth()"
